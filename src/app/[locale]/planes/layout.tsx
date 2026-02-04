48a9cd14ebe5d38118d54c505d97ec2f82ae922b
Joshuesito <jggjosue@gmail.com>
2026-02-04 10:33:03 -0700

    add: se agregan los keywords

--- src/app/[locale]/planes/layout.tsx
+++ src/app/[locale]/planes/layout.tsx
@@ -1,7 +1,48 @@
-import Header from '@/app/[locale]/components/header';
 import Footer from '@/app/[locale]/components/footer';
-import {NextIntlClientProvider} from 'next-intl';
-import {getMessages, setRequestLocale} from 'next-intl/server';
+import Header from '@/app/[locale]/components/header';
+import type { Metadata } from 'next';
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages, setRequestLocale } from 'next-intl/server';
+
+export const metadata: Metadata = {
+  title: 'ICIAR Nayarit',
+  description: 'Somos una Iglesia Cristiana Interdenominacional comprometida con compartir el amor de Cristo en Nayarit.',
+  keywords: [
+    'Cristianismo',
+    'ICIAR Iglesia Central - Portales',
+    'Doctrina',
+    'Himno',
+    'Logotipo',
+    'Himnario',
+    'Dios',
+    'Oración',
+    'Escuela dominical',
+    'Iglesia',
+    'Vida',
+    'Biblia',
+    'Himnario ICIAR',
+    'Rector',
+    'RISING',
+    'iglesia iciar',
+    'iciar portales',
+    'tabernáculo la mansión iciar',
+    'puntos doctrinales iciar',
+    'ici makis',
+    'ici hot',
+    'ici puebla',
+    'ici radio canada',
+    'ici tou tv',
+    'ici rdi',
+    'ici tout tv',
+    'bryan adams ma place est ici',
+    'ici première',
+    'cici',
+    'ici tele',
+    'ici rdi en direct',
+    'ici télé',
+    'ici tout.tv',
+  ],
+};
 
 export default async function PlanesLayout({
   children,
