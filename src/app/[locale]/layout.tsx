a87cc5b109e6958882c48bcba53bce56aecb4b20
Joshuesito <jggjosue@gmail.com>
2026-02-04 10:49:14 -0700

    add: se agrega el locale

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -48,11 +48,12 @@ export const metadata: Metadata = {
 
 export default async function RootLayout({
   children,
-  params: {locale}
+  params
 }: {
   children: React.ReactNode;
-  params: {locale: string};
+  params: Promise<{locale: string}>;
 }) {
+  const { locale } = await params;
   const messages = await getMessages();
   return (
       <NextIntlClientProvider locale={locale} messages={messages}>
--- src/app/[locale]/videos/layout.tsx
+++ src/app/[locale]/videos/layout.tsx
@@ -46,11 +46,12 @@ export const metadata: Metadata = {
 
 export default async function VideosLayout({
   children,
-  params: {locale}
+  params
 }: {
   children: React.ReactNode;
-  params: {locale: string};
+  params: Promise<{locale: string}>;
 }) {
+  const { locale } = await params;
   setRequestLocale(locale);
   const messages = await getMessages();
 
