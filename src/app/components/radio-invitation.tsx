234d03634b6bf3b7c91bde5df7c44a4a55fc1277
iciarnayarit <iciarnayarit@gmail.com>
2026-02-11 05:52:30 +0000

    add radio

--- src/app/components/radio-invitation.tsx
+++ src/app/components/radio-invitation.tsx
@@ -21,10 +21,16 @@ export default function RadioInvitation() {
       />
       <div className="absolute inset-0 bg-black/60 z-10"></div>
       <div className="relative container mx-auto px-4 md:px-6 text-center text-white z-20">
-        <h2 className="text-3xl font-bold font-headline mb-4">Escucha la radio en vivo y las predicaciones todos los días.</h2>
+        <h2 className="text-3xl font-bold font-headline mb-4">Sintoniza nuestra radio en vivo y edifícate con las predicaciones diarias.</h2>
+        <h1 className="text-3xl font-bold font-headline mb-4">¡Conéctate ahora!</h1>
         <p className="text-lg mb-8">
-          Los horarios programados de las predicaciones son: 7:00 a.m., 11:00 a.m., 3:00 p.m., 7:00 p.m. y 11:00 p.m.
+        Horario de Predicaciones <br />
         </p>
+        <ul>
+          <li>Mañana: 07:00 a.m. | 11:00 a.m.</li>
+          <li>Tarde: 03:00 p.m. | 07:00 p.m.</li>
+          <li>Noche: 11:00 p.m.</li>
+        </ul><br />
         <div className="flex justify-center gap-4">
           <Button onClick={handleWhatsAppClick} style={{ backgroundColor: 'purple', color: 'white' }}>
             <MessageSquareHeart className="mr-2 h-4 w-4" />
